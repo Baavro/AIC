3f119fe706823fe6ab28b5d9717812d9ee0cbf6d
Baavro <81712871+Baavro@users.noreply.github.com>
2023-11-04 12:16:42 +0530

    Update TimeLine.tsx

--- src/screens/TimeLine.tsx
+++ src/screens/TimeLine.tsx
@@ -3,46 +3,47 @@ import React from 'react';
 const steps = [
   {
     id: 1,
-    month: 'December, 2023',
+    month: 'November, 2023',
     name: 'Introduction and workshop',
     description:
-      'Events & workshops around technical aspects of Gen AI',
+      '1) Orientation workshop conducted by IITs at their own institute around the new AI Club. 2) Selecting team members and planning local activities for the year. 3) Approx 50-80 members are chosen to be a direct part of every AI Club',
   },
   {
     id: 2,
-    month: 'January, 2024',
-    name: 'Mentorship',
+    month: 'December, 2024',
+    name: 'Diving deeper',
     description:
-      'Each School Club to be assigned a mentor IIT',
+      '1) Events & workshops around technical aspects of Gen AI. 2) Increasing awareness around other aspects of Gen AI in the field of design, music, video and more. 3) Opening up Gen AI Clubs in 15 NITs, 3 IIITs & 5 private colleges
+',
   },
   {
     id: 3,
     month: 'January,2024',
-    name: 'Dive into the field',
+    name: 'Moonshot with IITians',
     description:
-      'Increasing awareness around other aspects of Gen AI in the field of design, music, video and more',
+      '1) Pan IIT Hackathon in 1st week of January with participation from 10-12 IITs and 1000 students. 2) Setting up more than 200-500 AI Clubs across high schools in India, these clubs will be mentored by the students from IITs. Each club to get a dedicated mentor AI Club from IIT',
   },
   {
     id: 4,
     month: 'March, 2024',
     name: 'Pan India Gen AI Hackathon',
     description:
-      'Pan India Gen AI Hackathon with participation from all AI Clubs with approx 5k participating students',
+      '1) Pan India Gen AI Hackathon with participation from all AI Clubs with approx 5k participating students. 2) AI fest locally in all IITs/NITs (coincides with their technical fest where more than 200k students participate in total)',
   },
   {
     id: 5,
-    month: 'March, 2024',
-    name: 'Flagship AI dest',
+    month: 'May-June, 2024',
+    name: 'Builders Zone',
     description:
-      'AI fest events & activities in all IITs/NITs to be made available to students of AI Clubs',
+      '1) Workshop every week around latest AI technologies, AI tools. 2) Building Projects around AI, a 30 day builders zone where all students pick up from a set of problem statements, form a team and build something cool and share progress.',
   },
-  // {
-  //   id: 6,
-  //   month: 'month 6',
-  //   name: 'Introduction and workshop',
-  //   description:
-  //     'By joining the Artificial Intelligence Council, institutes can benefit from a range of valuable resources and opportunities that includes sponsorships for fests and events.',
-  // },
+  {
+    id: 6,
+    month: 'August, 2024',
+    name: 'Freshers!',
+    description:
+      'Engaging new 1st year students (upcoming freshers) with respective AI Clubs',
+  },
 ];
 
 const Timeline = () => {
